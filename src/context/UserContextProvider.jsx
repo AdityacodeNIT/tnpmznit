1ad3f2ab964e263906aeaf68cb97c41fc9026954
AdityacodeNIT <srivastavaditya145@gmail.com>
2024-05-19 14:39:42 +0530

    Intial Setings

---
+++ src/context/UserContextProvider.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from "react";
+
+import UserContext from "./UserContext.js";
+
+const UserContextProvider=({Children})=>{
+
+
+
+return <UserContext.Provider value={{}}></UserContext.Provider>
+}
+export default UserContextProvider
